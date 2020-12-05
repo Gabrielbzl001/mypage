6a7a2c203880983fdecde10394e084d96f695861
Gabriel Lima <gbragazlima001@gmail.com>
2020-12-04 21:08:38 -0400

    Verificação

--- pages/sobre.js
+++ pages/sobre.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 function Sobre(){
     return (
     <div>
-        <h1>Sobre</h1>
+        <h1>Sobre 1</h1>
         <Link href="/index">
             <a>Acessar página Home</a>
         </Link>
