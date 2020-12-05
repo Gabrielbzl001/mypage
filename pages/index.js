d7eed0b0a77db568b682b494246aaa89607515af
Gabriel Lima <gbragazlima001@gmail.com>
2020-12-04 20:39:30 -0400

    Correção da Home

--- pages/index.js
+++ pages/index.js
@@ -4,25 +4,10 @@ function Home(){
     return (
     <div>
         <h1>Home 4</h1>
-        <Contador />
     </div>
 
     )
 }
 
-function Contador(){
-    const [contador, setContador] = useState(1);
-
-    function adicionarContador(){
-        setContador(contador + 1);
-    }
-
-    return(
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
 
 export default Home
