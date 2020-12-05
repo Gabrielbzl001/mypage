17afe13ceb9f26f9001e62829872d3131f0fce52
Gabriel Lima <gbragazlima001@gmail.com>
2020-12-04 21:53:29 -0400

    Add title

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,8 +1,14 @@
 import Link from 'next/link';
+import Head from "next/head"
+
 
 function Sobre(){
     return (
     <div>
+        <Head>
+            <title>Sobre</title>
+            <link rel="shortcut icon" href="../images/icons/favicon.ico" />
+        </Head>
         <h1>Sobre 2</h1>
         <Link href="/">
             <a>Acessar página Home</a>
