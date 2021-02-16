3a9852713ff1d5c2fdd7a08ecce515293d9b514e
Gabriel <g.braga@ufms.br>
2021-02-15 21:21:38 -0400

    adiciona produtos

---
+++ pages/produtos/[id].js
@@ -0,0 +1,31 @@
+function Produtos({id}){
+    return <div>Id do produto: {id}</div>
+}
+
+export async function getStaticPaths(){
+    return{
+        paths:[{
+            params:{
+                id: '1'
+            }
+        },{
+            params:{
+                id: '2'
+            }
+        }
+    ],
+        fallback: 'blocking',
+    }
+}
+
+export async function getStaticProps(context){
+    const {id} = context.params;
+
+    return{
+        props: {
+            id
+        }
+    }
+}
+
+export default Produtos;
